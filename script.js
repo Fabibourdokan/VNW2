e4bf22714c5d526710b35f584ab9db40f01ac6b6
Fabibourdokan <fabianabourdokanvaliente@gmail.com>
2022-07-07 19:08:19 -0300

    desafio entregue

---
+++ script.js
@@ -0,0 +1,20 @@
+let age= 99
+if (age>18)
+{console.log('você é velho')}
+else {console.log('você é criança')}
+
+let humano = false
+if (age>18 && humano === false){console.log('você não é extraterrestre')}
+else {console.log('você é terraquio')}
+
+let mes= 'novembro'
+if (mes == 'janeiro' || mes == 'julho') {console.log('sem presentes')}
+else {console.log('parabéns, você nasceu no melhor mês')}
+
+let name = "João"
+if (name.substring(0,1) == 'R'){console.log ('Seu nome começa com J')}
+else {console.log('seu nome começa com R')}
+
+let lastname = 'Silva'
+if(lastname.length>6 || name.substring(0,1) === 'S'){console.log('seu nome começa com S')}
+else {console.log('seu nome não começa com S')}
